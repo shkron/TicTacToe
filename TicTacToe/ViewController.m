8cf8cfb67ac47cd77bdf6c7fe4f777e71356412f
Alexey Emelyanov <emelyanoff@gmail.com>
2016-07-14 22:24:09 -0700

    Initial Commit

---
+++ TicTacToe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TicTacToe
+//
+//  Created by Alex on 7/14/16.
+//  Copyright © 2016 AE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
